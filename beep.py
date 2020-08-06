8685c3959e2da6923bf32d7085e2665d2e7071c3
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-08-06 23:53:23 +0530

    Update beep.py

--- beep.py
+++ beep.py
@@ -6,10 +6,11 @@ def main():
     sound_played = False
     while (True):
         battery = psutil.sensors_battery()
-        if (battery.power_plugged and battery.percent == 100 and not sound_played):
-            beepy.beep(sound=1)
-            sound_played = True
-            return
+        if (not battery.power_plugged or battery.percent != 100 and not sound_played):
+            continue
+        beepy.beep(sound=1)
+        sound_played = True
+        return
 
 if __name__ == "__main__":
     main()
